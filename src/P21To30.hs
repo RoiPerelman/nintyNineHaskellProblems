1d3a0e81ab17731e620231e5a69874cbc2a90a8b
Roi Perelman <roi@dynamicyield.com>
2017-10-14 13:56:37 +0300

    Problems 24-26 no tests

--- src/P21To30.hs
+++ src/P21To30.hs
@@ -2,11 +2,14 @@ module P21To30
     ( insertAt
     , range
     , rnd_select
+    , diff_select
+    , combinations
     ) where
 
 import qualified P11To20
 import Control.Monad
 import System.Random
+import Data.List
 
 -- Problem 21
 insertAt :: a -> [a] -> Int -> [a]
@@ -18,6 +21,7 @@ range :: Int -> Int -> [Int]
 range start end = [start .. end]
 
 -- Problem 23
+-- can select the same one twice
 rnd_select :: [a] -> Int -> IO [a]
 rnd_select [] _ = return []
 rnd_select xs n
@@ -25,4 +29,31 @@ rnd_select xs n
   | otherwise = do ys <- replicateM n $ getStdRandom $ randomR (0, length xs - 1)
                    return [xs!!y | y <- ys]
 
+-- can't select the same one twice
+rnd_select2 l count = getStdRandom $ rnd_select_helper l count
+
+rnd_select_helper :: RandomGen g => [a] -> Int -> g -> ([a], g)
+rnd_select_helper _ 0 gen = ([], gen)
+rnd_select_helper [] _ gen = ([], gen)
+rnd_select_helper l count gen
+   | count > (length l) = ([], gen)
+   | otherwise           =  rnd_select_helper (P11To20.removeAt l (k+1)) count gen'
+                            where (k, gen') =
+                                    randomR (0, (length l) - 1) gen
+
 -- Problem 24
+diff_select :: Int -> Int -> IO [Int]
+diff_select n m = rnd_select [1 .. m] n
+
+-- Problem 25
+rnd_permu :: [a] -> IO[a]
+rnd_permu xs = rnd_select2 xs $ length xs
+
+-- Problem 26
+-- for every subset find all the combinations and append them to the first index
+-- tails for [1,2,3] returns -> (1:2:3:[], 2:3:[], 3:[])
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = return []
+combinations n xs = do y:xs' <- tails xs
+                       ys <- combinations (n-1) xs'
+                       return (y:ys)
