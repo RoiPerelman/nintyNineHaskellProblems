07ef3fc6c7f77fec194a7cdb6e19d604d2edc945
Roi Perelman <roi@dynamicyield.com>
2017-10-16 11:48:48 +0300

    Problems 34 - 39

--- src/P31To41.hs
+++ src/P31To41.hs
@@ -15,3 +15,41 @@ coprime :: Int -> Int -> Bool
 coprime a b = myGCD a b == 1
 
 -- Problem 34
+totient :: Int -> Int
+totient n = sum $ map (\x -> if coprime n x then 1 else 0) [1..n-1]
+
+totient' :: Int -> Int
+totient' n = length [x | x <- [1..n], coprime x n]
+
+-- Problem 35
+findNextPrime :: Int -> Int
+findNextPrime a
+  | isPrime $ a + 1 = a + 1
+  | otherwise = findNextPrime $ a + 1
+
+primeFactors :: Int -> [Int]
+primeFactors n = primeFactorsHelper n 2
+  where
+    primeFactorsHelper n p
+      | isPrime n = [n]
+      | n `mod` p == 0 = p:primeFactorsHelper (n `div` p) p
+      | otherwise = primeFactorsHelper n $ findNextPrime p
+
+-- Problem 36
+prime_factors_mult :: Int -> [(Int, Int)]
+prime_factors_mult n = tail $ reverse $ foldl (\acc p ->
+                                if (fst . head $ acc) == p
+                                then (p, (snd . head $ acc) + 1):(tail acc)
+                                else (p, 1):acc)
+                       [(1,0)] $ primeFactors n
+
+-- Problem 37 - skipped
+-- Problem 38 - skipped
+-- Problem 39
+primesR :: Int -> Int -> [Int]
+primesR n1 n2
+  | n1 > n2 = []
+  | isPrime n1 = n1:(primesR (findNextPrime n1) n2)
+  | otherwise = primesR (findNextPrime n1) n2
+
+-- Problem 40
--- test/P31To41Spec.hs
+++ test/P31To41Spec.hs
@@ -25,20 +25,20 @@ spec = do
       it "should return True fro 35 and 64" $ do
         coprime 35 64 `shouldBe` (True)
 
-  -- describe "Problem 14 - Duplicate the elements of a list." $ do
-  --   context "dupli" $ do
-  --     it "should duplicate each character" $ do
-  --       dupli "abccd" `shouldBe` ("aabbccccdd")
+  describe "Problem 24 - Calculate Euler's totient function phi(m)." $ do
+    context "totient" $ do
+      it "should return the number of coprime numbers to n" $ do
+        totient 10 `shouldBe` (4)
 
-  -- describe "Problem 15 - Replicate the elements of a list a given number of times." $ do
-  --   context "repli" $ do
-  --     it "should replicate 3 times" $ do
-  --       repli "abc" 3 `shouldBe` ("aaabbbccc")
+  describe "Problem 25 - Determine the prime factors of a given positive integer. Construct a flat list containing the prime factors in ascending order." $ do
+    context "primeFactors" $ do
+      it "should return prime factors of an integer" $ do
+        primeFactors 315 `shouldBe` ([3, 3, 5, 7])
 
-  -- describe "Problem 16 - Drop every N'th element from a list." $ do
-  --   context "dropEvery" $ do
-  --     it "should drop every third element" $ do
-  --       dropEvery "abcdefghik" 3 `shouldBe` ("abdeghk")
+  describe "Problem 36 - Determine the prime factors of a given positive integer." $ do
+    context "prime_factors_mult" $ do
+      it "should get a list containing the prime factors and their multiplicity" $ do
+        prime_factors_mult 315 `shouldBe` ([(3,2),(5,1),(7,1)])
 
   -- describe "Problem 17 - Split a list into two parts; the length of the first part is given" $ do
   --   context "split" $ do
@@ -50,11 +50,10 @@ spec = do
   --     it "should give a list given 2 indices" $ do
   --       slice "abcdefghik" 3 7 `shouldBe` ("cdefg")
 
-  -- describe "Problem 19 - Rotate a list N places to the left" $ do
-  --   context "rotate" $ do
-  --     it "should get a number and rotate the arr times that number" $ do
-  --       rotate "abcdefgh" 3 `shouldBe` ("defghabc")
-  --       rotate "abcdefgh" (-2) `shouldBe` ("ghabcdef")
+  describe "Problem 39 - A list of prime numbers" $ do
+    context "primesR" $ do
+      it "should give all prime numbers in range" $ do
+        primesR 10 20 `shouldBe` ([11, 13, 17, 19])
 
   -- describe "Problem 19 - Remove the K'th element from a list." $ do
   --   context "removeAt" $ do
